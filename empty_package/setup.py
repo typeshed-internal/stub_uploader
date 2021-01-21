ea43573ae6093d34bc731f3d8d4518ed85446a57
Ivan Levkivskyi <levkivskyi@gmail.com>
2021-01-21 16:42:36 +0000

    Create an empty package for testing

---
+++ empty_package/setup.py
@@ -0,0 +1,19 @@
+import sys
+from setuptools import setup
+
+increment = int(sys.argv[1])
+name = "types-tsbot-empty"
+description = "Empty typing stubs for testing typeshed/PyPI auto-upload"
+
+setup(
+    name=name,
+    version=f"0.1.{increment}",
+    description=description,
+    long_description=description,
+    url="https://github.com/typeshed-internal/stub_uploader",
+    install_requires=[],
+    packages=[],
+    classifiers=[
+        "Typing :: Typed",
+    ]
+)
