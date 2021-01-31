87c30a202eb7be54988bbd56e3c196aee885605f
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2021-01-31 13:58:42 +0000

    Add some more tests

--- tests/integration.py
+++ tests/integration.py
@@ -4,9 +4,11 @@ anything on PyPI, but can make PyPI queries and may expect
 a typeshed checkout side by side.
 """
 import os
+import pytest
 from scripts import get_version, build_wheel
 
 TYPESHED = "../typeshed"
+UPLOADED = "data/uploaded_packages.txt"
 
 
 def test_version() -> None:
@@ -28,3 +30,16 @@ def test_build_wheel() -> None:
         tmp_dir = build_wheel.main(TYPESHED, distribution, increment=1)
         assert tmp_dir.endswith("/dist")
         assert list(os.listdir(tmp_dir))  # check it is not empty
+
+
+def test_verify_dependency() -> None:
+    # Check some known dependencies that they verify as valid.
+    build_wheel.verify_dependency(TYPESHED, "types-six", UPLOADED)
+    build_wheel.verify_dependency(TYPESHED, "types-six==0.1.1", UPLOADED)
+    build_wheel.verify_dependency(TYPESHED, "types-typing-extensions", UPLOADED)
+    build_wheel.verify_dependency(TYPESHED, "types-typing-extensions>=3.7", UPLOADED)
+    # Also check couple errors.
+    with pytest.raises(AssertionError):
+        build_wheel.verify_dependency(TYPESHED, "unsupported", UPLOADED)
+    with pytest.raises(AssertionError):
+        build_wheel.verify_dependency(TYPESHED, "types-unknown-xxx", UPLOADED)
