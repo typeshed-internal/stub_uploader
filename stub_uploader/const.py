31928967ee05172fdde3efcb06f22a44bf0790d4
Sebastian Rittau <sebastian.rittau@zfutura.de>
2026-08-31 14:53:05 +0200

    Move the external depenedcy allowlists to a more visible place

--- stub_uploader/const.py
+++ stub_uploader/const.py
@@ -18,3 +18,54 @@ TYPE_CHECKERS = [
     ("pyright", "https://microsoft.github.io/pyright/"),
     ("ty", "https://docs.astral.sh/ty/"),
 ]
+
+# The following non-typeshed packages are allowed to appear in the
+# `dependencies` or `optional-dependencies` dependencies field of a package.
+#
+# Be conservative on what to add here. typeshed packages are generally
+# trusted by users and should not depend on packages with a low trust
+# level. Presence in the top 1000 PyPI packages could be a necessary but not
+# sufficient criterion for inclusion in this allowlist.
+#
+# Note we could loosen our criteria once we address:
+# https://github.com/typeshed-internal/stub_uploader/pull/61#discussion_r979327370
+EXTERNAL_REQ_ALLOWLIST = {
+    "Flask",
+    "Flask-SQLAlchemy",
+    "MarkupSafe",
+    "Pillow",
+    "Werkzeug",
+    "affine",
+    "arrow",
+    "asgiref",
+    "beautifulsoup4",
+    "click",
+    "cryptography",
+    "django-stubs",
+    "djangorestframework-stubs",
+    "httpx",
+    "matplotlib",
+    "numpy",
+    "pandas-stubs",
+    "pygobject-stubs",
+    "pynacl",
+    "pyproj",
+    "pytest",
+    "referencing",
+    "requests",
+    "setuptools",
+    "torch",
+    "tree-sitter",
+    "urllib3",
+    "websockets",
+    "wsproto",
+}
+
+# Map of external stub packages to their runtime equivalent.
+# We check that the stubs actually depend on their runtime package.
+EXTERNAL_RUNTIME_REQ_MAP = {
+    "django-stubs": "django",
+    "djangorestframework-stubs": "djangorestframework",
+    "pandas-stubs": "pandas",
+    "pygobject-stubs": "pygobject",
+}
